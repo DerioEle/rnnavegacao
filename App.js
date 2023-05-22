5c2de08f9c431ac21e870e4a5f19701f9381e9be
Dério Oliveira <dco.ele21@uea.edu.br>
2023-05-22 14:37:17 -0400

    update1

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import useProdutores from './src/hooks/useProdutores';
 
 import Home from './src/telas/Home';
-import Cesta from './src/telas/Cesta';
+import MelhoresProdutores from './src/telas/MelhoresProdutores';
+
 
 export default function App() {
   const produtores = useProdutores(false);
 
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
-    <Home melhoresProdutores={false} />
-    {/* {produtores.length > 0 && 
-      <Cesta produtor={{
-        nome: produtores[0].nome, 
-        imagem: produtores[0].imagem
-      }}
-      {...produtores[0].cestas[0]} />
-    }  */}
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
+    
   </SafeAreaView>
 }
