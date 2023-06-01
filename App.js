5796872b70abd8df95fba4511ddb0d1888b65d5f
Dério Oliveira <dco.ele21@uea.edu.br>
2023-06-01 17:47:44 -0400

    Produtor-Parte1

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import useProdutores from './src/hooks/useProdutores';
-
-import Home from './src/telas/Home';
-import MelhoresProdutores from './src/telas/MelhoresProdutores';
-
+import AppRotas from './src/rotas/AppRotas';
 
 export default function App() {
-  const produtores = useProdutores(false);
-
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
-    <NavigationContainer>
-      <Home />
-    </NavigationContainer>
-    
+    <AppRotas />
   </SafeAreaView>
 }
---
+++ src/rotas/AppRotas.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
+import ProdutorRotas from "../rotas/ProdutorRotas"
+
+const Tab = createBottomTabNavigator();
+
+export default function AppRotas(){
+    return <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name='Home' component={ProdutorRotas} />
+      <Tab.Screen name='Melhores produtores' component={MelhoresProdutoresRotas} />
+    </Tab.Navigator>
+  </NavigationContainer>
+}
