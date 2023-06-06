4a1a286c0eec5795e188ba7c7f9c7ed0bc1059d6
Dério Oliveira <dco.ele21@uea.edu.br>
2023-06-06 17:27:54 -0400

    Estilização

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -5,11 +5,33 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 import ProdutorRotas from "../rotas/ProdutorRotas"
 
+import Coracao from "../assets/coracao.svg";
+import Home from "../assets/home.svg";
+
 const Tab = createBottomTabNavigator();
+let tamanho = 20
 
 export default function AppRotas(){
     return <NavigationContainer>
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator screenOptions={({ route }) => ({ 
+      headerShown: false,
+      tabBarIcon: ({ color }) => {
+        let Icon = Home;
+
+        if (route.name === "Melhores produtores"){
+          Icon = Coracao;
+        }
+
+        return <Icon color={color} width={tamanho} height={tamanho} />
+      },
+      tabBarActiveTintColor: "#2A9F85",
+      tabBarInactiveTintColor: "#C7C7C7",
+      tabBarLabelStyle: {
+        fontSize: 15,
+      },
+
+
+      })}>
       <Tab.Screen name='Home' component={ProdutorRotas} />
       <Tab.Screen name='Melhores produtores' component={MelhoresProdutoresRotas} />
     </Tab.Navigator>
